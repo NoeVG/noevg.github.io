9ace97d42972ed7367b2133531aa1e596ba9ede8
NoeVG <noe-vg@outlook.com>
2021-07-18 21:48:23 -0500

    update page

---
+++ js/control.js
@@ -0,0 +1,9 @@
+
+window.onload = function() {
+    runControl();
+};
+  
+function runControl() {
+    //var ax = document.getElementById("acX").innerHTML = 6;    
+    //var ax = document.getElementById("acX");    
+}
